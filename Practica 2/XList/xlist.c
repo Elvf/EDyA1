fe1edc9bc281f2bcb3e44013a6ddcfc953278aae
Felipe <ftrcsg@gmail.com>
2017-04-28 20:17:42 -0300

    Agregada xlist_destroy
    Oops!

--- Practica 2/XList/xlist.c
+++ Practica 2/XList/xlist.c
@@ -14,7 +14,18 @@ XList xlist_xor(XNodo *nodo1, XNodo *nodo2) {
 void xlist_destruir(XList lista) {
   if (lista) {
     if (xlist_xor(NULL, lista->npx) != NULL) {
-      
+      XNodo *actual = xlist_xor(NULL, lista->npx);
+      XNodo *prev = lista;
+      XNodo *next;
+
+      while (actual) {
+          next = xlist_xor(prev, actual->npx);
+          free(prev);
+          prev = actual;
+          actual = next;
+      }
+
+      free(lista);
     } else free(lista);
   }
 }
@@ -37,15 +48,15 @@ XList xlist_agregar_final(XList lista, int dato) {
       prev = actual;
       actual = next;
     }
-    
+
     nuevoNodo->npx = xlist_xor(prev, NULL);
     ant = xlist_xor(prev->npx, NULL);
     prev->npx = xlist_xor(ant, nuevoNodo);
-    
+
     return lista;
   } else {
     nuevoNodo->npx = xlist_xor(NULL, NULL);
-    
+
     return nuevoNodo;
   }
 }
@@ -53,14 +64,14 @@ XList xlist_agregar_final(XList lista, int dato) {
 XList xlist_agregar_inicio(XList lista, int dato) {
   XNodo *nuevoNodo = malloc(sizeof(XNodo));
   nuevoNodo->dato = dato;
-  
+
   nuevoNodo->npx = xlist_xor(lista, NULL);
-  
+
   if (lista) {
     XNodo *siguiente = xlist_xor(lista->npx, NULL);
     lista->npx = xlist_xor(nuevoNodo, siguiente);
   }
-  
+
   return nuevoNodo;
 }
 
@@ -71,7 +82,7 @@ void xlist_recorrer(XList lista, FuncionVisitante visitar) {
 
   while (actual) {
     visitar(actual->dato);
-    
+
     next = xlist_xor(prev, actual->npx);
     prev = actual;
     actual = next;
